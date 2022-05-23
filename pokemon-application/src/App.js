db55f730e49486faee3099e69c1ce84c09e3618a
hemagunti <hema.gunti@gmail.com>
2022-05-23 18:33:25 +0200

    fetch pokemon api

--- pokemon-application/src/App.js
+++ pokemon-application/src/App.js
@@ -2,6 +2,24 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [limit, setlimit] = useState(10);
+  const [offset, setoffset] = useState(10);
+  const [load, setLoad] = useState(0);
+
+  const listPokemons = async () => {
+    setPokemons([]);
+    setLoad(0);
+    const api = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+    const res = await fetch(api);
+    const data = await res.json();
+    console.log("data",data);
+}
+
+  useEffect(() => {
+    listPokemons();
+  }, [limit, offset]);
+
   return <div className="App">
     <h2>Pokemon Application</h2>
   </div>;
